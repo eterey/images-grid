7f29b19ddf842d61fe6131f0acc2ee28f43a9246
maxim <uzumaxy@gmail.com>
2015-03-29 13:25:14 +0300

    Build test grid.

--- app/client/views/home/home.js
+++ app/client/views/home/home.js
@@ -26,6 +26,37 @@ Template.Home.created = function () {
 };
 
 Template.Home.rendered = function () {
+  $('#images-grid').kendoGrid({
+    dataSource: {
+      type: 'odata',
+      transport: {
+        read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Customers"
+      },
+      pageSize: 20
+    },
+    height: 550,
+    groupable: true,
+    sortable: true,
+    pageable: {
+      refresh: true,
+      pageSizes: true,
+      buttonCount: 5
+    },
+    columns: [{
+      field: "ContactName",
+      title: "Contact Name",
+      width: 200
+    }, {
+      field: "ContactTitle",
+      title: "Contact Title"
+    }, {
+      field: "CompanyName",
+      title: "Company Name"
+    }, {
+      field: "Country",
+      width: 150
+    }]
+  });
 };
 
 Template.Home.destroyed = function () {
